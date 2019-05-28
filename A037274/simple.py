69967ef504bf1d5d25739f464bc1590e856e5721
Seth Troisi <sethtroisi@google.com>
2019-05-28 16:40:20 -0700

    Done with up to 5,000

--- A037274/simple.py
+++ A037274/simple.py
@@ -1,9 +1,7 @@
 import gmpy2
-import itertools
 import subprocess
 import math
 import time
-
 
 from factordb.factordb import FactorDB
 
@@ -34,28 +32,42 @@ def factor_large(n, b1=10**6):
   return list(map(int, result.stdout.strip().split()))
 
 
-def attempt_factorization(s, know_factors):
-  t = s
+def trial_factor(t, primes, original):
+  t_sqrt = math.sqrt(t)
+
   factors = []
+  for p in primes:
+    if p > t_sqrt:
+      break
 
-  for factor in know_factors:
-    assert gmpy2.is_prime(factor)
-    t //= factor
-    factors.append(factor)
+    while t > 0 and t % p == 0:
+      factors.append(p)
+      t //= p
+      t_sqrt = math.sqrt(t)
 
-  #if False:
-  if t >= 1e60:
-    # Check factorDB (probably already been done)
+      if t in original or gmpy2.is_prime(t):
+        factors.append(t)
+        t //= t
+        t_sqrt = 1
+
+  return t, factors
+
+
+def attempt_factorization(s, primes, original):
+  # This is the slowest part of rerunning
+  t, factors = trial_factor(s, primes, original)
+
+  if t >= 1e40:
+    # Check factorDB
     time.sleep(0.2)
     factordb = FactorDB(s)
     factordb.connect()
     factordb_factors = factordb.get_factor_list()
     if factordb_factors and factordb_factors != [s]:
       print ("\t\tfactordb:", factordb.get_status(), factordb_factors)
-      for factor in factordb_factors:
-        if gmpy2.is_prime(factor):
-          t //= factor
-          factors.append(factor)
+      if factordb.get_status() == "FF":
+        assert set(factors) < set(factordb_factors)
+        return 1, factordb_factors
 
   if t > 1 and t < 1e40:
     print ("\t\tpre:", factors, t)
@@ -76,11 +88,13 @@ def attempt_factorization(s, know_factors):
   return t, factors
 
 
-def load_from_file():
-  home_primes = defaultdict(list)
+# For use with kernprof -v --line-by-line simple.py
+#@profile
+def run():
+  with open("factors") as f:
+    extra_primes = sorted(map(int, f.readlines()))
 
-  n = None
-  s = None
+  home_primes = defaultdict(list)
   with open("home_primes.txt") as f:
     # each line is "<base> <start> <step> <status>: <factor> <factor> ..."
     for line in f.readlines():
@@ -88,48 +102,36 @@ def load_from_file():
       *pre, status = pre.split()
       base, start, step, = map(int, pre)
 
-      if start != n:
-        n = start
-        s = n
-
       factors = list(map(int, post.split()))
+      home_primes[(base, start, step)] = factors
+
       if status not in ("FF", "P"):
         print (line)
-      else:
-        home_primes[(base, start, step)] = factors
-        assert product(factors) == s, (start, step, s, factors)
-        s = int("".join(map(str, factors)))
 
+  # copy to see if we need to write new_factors
+  original = set(extra_primes)
 
-  all_primes = set()
-  for factors in home_primes.values():
-    for p in factors:
-      if gmpy2.is_prime(p):
-        all_primes.add(p)
+  print ("Found", len(extra_primes), "extra primes")
+  for p in extra_primes:
+    assert gmpy2.is_prime(p), p
+  print ("Verified")
 
-  print ("Found", len(all_primes), "primes")
-  return home_primes, all_primes
-
-
-# For use with kernprof -v --line-by-line simple.py
-#@profile
-def run():
-  home_primes, all_primes = load_from_file()
+  primes = [2]
+  while primes[-1] < 10 ** 4:
+    primes.append(int(gmpy2.next_prime(primes[-1])))
+  primes = sorted(set(primes) | set(extra_primes))
 
   required_steps = Counter()
   max_prime = Counter()
   try:
-    for n in range(2, 100):
+    for n in range(3500, 10000):
       print (n)
+      step = 1
       t = n
-      for step in itertools.count(0):
-        if gmpy2.is_prime(t):
-          break
-
+      while not gmpy2.is_prime(t):
         s = t
 
-        original = home_primes[(10, n, step)]
-        t, factors = attempt_factorization(s, original)
+        t, factors = attempt_factorization(t, primes, original)
 
         if t > 1:
           print ("Breaking, failed to factor C{}: {}".format(len(str(t)), t))
@@ -141,26 +143,27 @@ def run():
         new = int("".join(map(str, factors)))
         t = new
 
-        if step >= 0 or gmpy2.is_prime(s):
+        if step > 40 or gmpy2.is_prime(t):
           if new < 1e40:
             print ("\t", step, new, "from", s, factors)
           else:
             print ("\t", step, new)
             print ("\t\tfrom", factors)
 
-        if factors != original:
-          home_primes[(10, n, step)] = factors
-          print ("\t\tnew factor", factors)
+        home_primes[(10, n, step-1)] = factors
+
+        extra_primes += [f for f in factors if f not in extra_primes]
 
         # Size of 2nd largest prime
         max_prime[len(str(factors[-2:][0]))] += 1
+        step += 1
 
-      if gmpy2.is_prime(t):
-        home_primes[(10, n, step)] = [t]
-        required_steps[step] += 1
-      else:
+      if not gmpy2.is_prime(t):
         required_steps[1000] += 1
         print ("\t {} Gave({}th time) up on step {}".format(n, required_steps[1000], step))
+      else:
+        home_primes[(10, n, step-1)] = [t]
+        required_steps[step] += 1
 
   except KeyboardInterrupt:
     print("Stopping from ^C")
@@ -168,9 +171,6 @@ def run():
   with open("home_primes.txt", "w") as f:
     for base, start, step in sorted(home_primes.keys()):
       factors = home_primes[(base, start, step)]
-      if not factors:
-        continue
-
       if all(gmpy2.is_prime(f) for f in factors):
         if len(factors) == 1:
           status = "P"
