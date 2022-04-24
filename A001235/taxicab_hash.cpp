5283c53c4cffb194fc3c8d9d0a6b60054d811018
Seth Troisi <sethtroisi@google.com>
2022-04-24 16:09:38 -0700

    A001235 - Initial taxicab code

---
+++ A001235/taxicab_hash.cpp
@@ -0,0 +1,184 @@
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<unordered_set>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+void findHardyNumbers(long maxA)
+{
+	double oneThird = ((double) 1) / ((double) 3);
+	long maxA3B3 = maxA * maxA * maxA;
+	unordered_set<long> taxiCabNumbers;
+	unordered_set<long> a3b3;
+	for (long a = 0; ; a ++)
+	{
+		long cubeA = a * a * a;
+		long nextCubeA = (a+1) * (a+1) * (a+1);
+
+		if (cubeA > maxA3B3)
+			break;
+
+        // This should maybe grow if precent removed isn't large enough.
+		if (a3b3.size() > 5'000'000)
+		{
+			cout << "a3b3 has to many items, at a = " << a << endl;
+			int count = 0;
+			unordered_set<long>::iterator iter;
+			for (iter = a3b3.begin(); iter != a3b3.end(); ) {
+				if ((*iter) < cubeA) {
+					iter = a3b3.erase(iter);
+					count += 1;
+				} else {
+                    iter++;
+                }
+            }
+			cout << "\tremoved " << count << " elements, new size:" << a3b3.size()
+                 << "\tremoved: " << (1000LL * count) / (count + a3b3.size()) / 10.0 << "%" << endl;
+		}
+
+		long maxB = ((cubeA + cubeA) < maxA3B3) ? a : (long) pow((double) maxA3B3 - cubeA, oneThird);
+
+		for (long b = 0; b <= maxB; b ++)
+		{
+			long temp = cubeA + b * b * b;
+			if (a3b3.find(temp) != a3b3.end())
+				taxiCabNumbers.insert(temp);
+			else
+				if (temp > nextCubeA)
+					a3b3.insert(temp);
+		}
+	}
+
+    cout << "\tmaxA: " << maxA << "\ta3b3 elements: " << a3b3.size() << endl;
+
+	a3b3.clear();
+
+	long taxiCabArray[taxiCabNumbers.size()];
+	int length = 0;
+
+	unordered_set<long>::iterator iter;
+	for (iter = taxiCabNumbers.begin(); iter != taxiCabNumbers.end(); iter ++)
+		taxiCabArray[length++] = (*iter);
+
+	taxiCabNumbers.clear();
+
+	sort(taxiCabArray, taxiCabArray + length);
+
+	cout << endl << endl << "SORTED all " << length << " taxicab numbers." << endl;
+}
+
+long nthHardyNumber(long N)
+{
+	long maxA3B3 = 10000LL * 10000LL * 10000LL;
+
+	unordered_set<long> taxiCabNumbers;
+	unordered_set<long> a3b3;
+
+	for (long a = 0; ; a++)
+	{
+		long cubeA = a * a * a;
+		long nextCubeA = (a+1) * (a+1) * (a+1);
+
+		if (cubeA > maxA3B3)
+			break;
+
+		if (a3b3.size() > 8'000'000)
+		{
+			cout << "a3b3 has to many items, at a = " << a << endl;
+			int count = 0;
+			unordered_set<long>::iterator iter;
+			for (iter = a3b3.begin(); iter != a3b3.end(); ) {
+				if ((*iter) < cubeA) {
+					iter = a3b3.erase(iter);
+					count += 1;
+				} else {
+                    iter++;
+                }
+            }
+			cout << "\tremoved " << count << " elements, new size:" << a3b3.size()
+                 << "\tremoved: " << (1000LL * count) / (count + a3b3.size()) / 10.0 << "%" << endl;
+		}
+
+		for (long b = 0; b <= a; b ++)
+		{
+			long cubeB = b * b * b;
+
+			long temp = cubeA + cubeB;
+
+			if (temp > maxA3B3)
+				break;
+
+			if (a3b3.find(temp) != a3b3.end())
+			{
+				taxiCabNumbers.insert(temp);
+
+				int found = taxiCabNumbers.size();
+				if (found == N)
+				{
+					unordered_set<long>::iterator iter;
+
+					maxA3B3 = 0;
+					for (iter = taxiCabNumbers.begin(); iter != taxiCabNumbers.end(); iter ++)
+						if ((*iter) > maxA3B3)
+							maxA3B3 = (*iter);
+					cout << endl << "FOUND N MANY, max is " << maxA3B3 << endl << endl;
+				}
+			}
+			else
+				if (temp > nextCubeA)
+					a3b3.insert(temp);
+		}
+	}
+
+	a3b3.clear();
+
+	long taxiCabArray[taxiCabNumbers.size()];
+	int length = 0;
+
+	unordered_set<long>::iterator iter;
+	for (iter = taxiCabNumbers.begin(); iter != taxiCabNumbers.end(); iter ++)
+		taxiCabArray[length++] = (*iter);
+
+	taxiCabNumbers.clear();
+
+	sort(taxiCabArray, taxiCabArray + length);
+
+	cout << endl << endl << "SORTED all " << length << " taxicab numbers." << endl;
+
+	return taxiCabArray[N-1];
+}
+
+
+int main(int argc, char** argv)
+{
+    int maxA = 10000;
+	findHardyNumbers(maxA);
+
+    // Slower? but doesn't require guestimate on A.
+	int n = 40000;
+	cout << "n" << n << ": " << nthHardyNumber(n) << endl;
+
+
+
+    /*
+	if (argc != 2)
+	{
+		cout << "takes one paramater (output file)" << endl;
+		return -1;
+	}
+    */
+
+//	ofstream outFile;
+//	outFile.open(fileName);
+//	for (int index = 0; index < length; index ++)
+//	{
+//		cout << (index + 1) << ": " << taxiCabArray[index] << endl;
+//		outFile << (index + 1) << ": " << taxiCabArray[index] << endl;
+//	}
+//	outFile.close();
+
+	return 0;
+}
---
+++ A001235/taxicab_queue.cpp
@@ -0,0 +1,82 @@
+#include<cassert>
+#include<cstdio>
+#include<queue>
+
+struct item {
+    long a3b3;
+    long b;
+};
+
+
+class CompareItem {
+    public:
+    bool operator()(item &i1, item &i2)
+    {
+        return i1.a3b3 > i2.a3b3;
+    }
+};
+
+int main(void)
+{
+    int  count = 0;
+    long iters = 0;
+
+    /**
+     * This program is majorily limitted by O(log(n)) inserts in items
+     * Extra branches / logic doesn't slow us down much
+     *
+     * On a Ryzen 3900x -> 12 - 15 million iters/second (pop + push)
+     * 50,000 th pair (1601017842872) ~3.5 seconds
+     * 1,000,000 th pair (1376426637022528) ~400 seconds
+     */
+    std::priority_queue<item, std::vector<item>, CompareItem> items;
+
+    item i;
+    items.push({1, 0}); // (1, 0) -> 1
+
+    long next_a = 2;
+    long next_a3 = next_a * next_a * next_a;
+
+    long lastlast = -1;
+    long last = -1;
+    while (++iters)
+    {
+        i = items.top();
+        items.pop();
+
+        // Need to insert (next_a, 0)
+        if (i.b == 0)
+        {
+            items.push({next_a3, 0});
+            next_a++;
+            next_a3 = next_a * next_a * next_a;
+        }
+
+        if (i.a3b3 == last && last != lastlast)
+        {
+            count++;
+            if (count % 1000 == 0)
+                printf("\t%6dth %12lu  (b: %4lu size: %lu  iters: %lu)\n",
+                        count, i.a3b3, i.b, items.size(), iters);
+
+            if (count == 1000000)
+                break;
+        }
+        lastlast = last;
+        last = i.a3b3;
+
+        // (a, b) -> (a, b+1)
+        // (b+1)^3 - b^3 = 1 + 3*b + 3*b^2 = 1 + 3*(b + b*b)
+        long b2 = i.b * i.b;
+        long b3 = i.b * b2;
+        if ( (b3 << 1) < i.a3b3 )
+        {
+            long b3_delta = 1 + 3ul * (i.b + b2);
+            i.b++;
+            i.a3b3 += b3_delta;
+            items.push(i);
+        }
+
+    }
+    return 0;
+}
