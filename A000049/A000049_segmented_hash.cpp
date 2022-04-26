f7e2cd3b4494cadcd5cbdff86a1f964dbdcaf460
Seth Troisi <sethtroisi@google.com>
2022-04-25 19:23:01 -0700

    A000049 - Segmented hash is fast!

--- A000049/A000049_segmented_hash.cpp
+++ A000049/A000049_segmented_hash.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cassert>
 #include <chrono>
+#include <cmath>
 #include <cstdint>
 #include <cstdio>
 #include <iostream>
@@ -9,8 +10,6 @@
 #include <unordered_set>
 #include <utility>
 #include <vector>
-
-// #include "flat_hash_map.hpp"
 
 using std::vector;
 
@@ -24,11 +23,10 @@ using std::endl;
 // {x, y}
 typedef vector<std::pair<uint32_t, uint32_t>> congruence;
 
-typedef std::unordered_set<uint64_t> Set;
-//typedef std::set<uint64_t> Set;
-//typedef std::vector<uint64_t> Set;
-//typedef std::priority_queue<uint64_t, std::vector<uint64_t>> Set;
-//typedef   ska::flat_hash_set<uint64_t> Set;
+//typedef std::unordered_set<uint64_t> Set;
+//typedef std::unordered_set<uint32_t> Set;
+ #include "flat_hash_map.hpp"
+ typedef   ska::flat_hash_set<uint32_t> Set;
 
 /**
  * Expand one congruence class of the population
@@ -37,13 +35,20 @@ typedef std::unordered_set<uint64_t> Set;
  */
 std::pair<uint64_t, uint64_t>
 expand_class(
-        uint64_t N, uint64_t base, uint64_t residual,
+        uint64_t N, uint64_t mod_base, uint64_t residual,
+        size_t num_passes,
         Set &found,
         congruence &parts) {
 
-    uint64_t four_base_squared = 4ul * base * base;
-    uint64_t eight_base = 8ul * base;
-    uint64_t eight_base_squared = 8ul * base * base;
+    size_t shift = 0;
+    while (2ul << (++shift) <= mod_base);
+    // X >> shift is unique if X is a multiple of mod_base
+    assert((1ul << shift) <= mod_base);
+    assert((2ul << shift) > mod_base);
+
+    uint64_t four_base_squared = 4ul * mod_base * mod_base;
+    uint64_t eight_base = 8ul * mod_base;
+    uint64_t eight_base_squared = 8ul * mod_base * mod_base;
 
     // Do several passes over the data
     // On each pass
@@ -55,7 +60,7 @@ expand_class(
     vector<std::pair<uint64_t, uint32_t>> X;
     {
         for (const auto& d : parts) {
-            for (uint32_t x = d.first; ; x += base) {
+            for (uint32_t x = d.first; ; x += mod_base) {
                 uint64_t temp_x = 3ul * x * x;
                 if (temp_x > N)
                     break;
@@ -63,56 +68,63 @@ expand_class(
                 X.push_back({temp_x, d.second});
             }
         }
-        std::sort(X.begin(), X.end());
+        // To slow to be valueable
+        //std::sort(X.begin(), X.end());
         if (residual == 1)
             printf("\t%lu X values\n", X.size());
 
         parts.clear();
     }
 
+    // Max item that can be inserted/added to found
+    const size_t found_max_element = std::numeric_limits<
+        typename std::remove_reference_t<decltype(found)>::key_type
+    >::max();
+
     uint64_t found_count = 0;
     uint64_t enumerated = 0;
-
-    /**
-     * Large values reduce Hash size
-     * BUT increase number of iterations over X
-     * Aim for a 5-10 values of y per pass?
-     */
-    size_t num_passes = 4;
 
     for (size_t pass = 0; pass < num_passes; pass++) {
         // Count number of values [pass_min, pass_max];
         size_t pass_min = (__uint128_t) N * pass / num_passes + 1;
         size_t pass_max = (__uint128_t) N * (pass + 1) / num_passes;
+        if (pass == 0) {
+            pass_min = 0;
+        }
+
         size_t pass_enumerated = 0;
+
+        const size_t max_element = (pass_max - pass_min) >> shift;
+        assert(max_element <= found_max_element);
 
         for(auto& d : X) {
             /* 3*x^2 */
             uint64_t temp_x = d.first;
-            if (temp_x > pass_max)
-                break;
+  //          if (temp_x > pass_max) Requires sort(X) which is slow
+  //              break;
 
-            uint64_t y = d.second;
+            uint32_t y = d.second;
 
             // 4 * ((y + base)^2 - y^2) = 8*base*y + 4*base^2
             // derivative with y and y+base => 8*base*base
-            uint64_t temp_y = (y * y) << 2;
+            uint64_t temp_y = ((uint64_t) y * y) << 2;
 
             uint64_t n = temp_x + temp_y;
             uint64_t y_delta = eight_base * y + four_base_squared;
 
             for (; n <= pass_max;) {
-                //assert(n % base == residual);
+                //assert(n % mod_base == residual);
+//                assert(n >= pass_min);
+//                assert(n <= pass_max);
 
-                found.insert(n);
-                //found.push_back(n);
-                //found.push(n);
+//                found.insert(n);
+                found.insert((n - pass_min) >> shift);
                 pass_enumerated++;
 
                 // N takes value for new y, but not inserted into found yet
                 n += y_delta;
                 y_delta += eight_base_squared;
-                y += base;
+                y += mod_base;
             }
 
             // Save progress to y
@@ -189,11 +201,13 @@ int main(int argc, char** argv)
 
     uint64_t N = 1ull << bits;
 
+
     // All congruence classes are only possible if num_classes is a prime
     //      4*k + 1 -> quadratic residual -> twice as many entries for 0
     //      4*k + 3 -> none quad residual -> 1 entry for 0
     // 37, 101, 331, 1009, 3343, 10007, 30011
     uint64_t num_classes = 10007; //30011;
+
 
     vector<congruence> classes = build_congruences(N, num_classes);
 
@@ -206,54 +220,55 @@ int main(int argc, char** argv)
     printf("\telements: %'lu (<= %lu)\n", elements, num_classes * num_classes);
     printf("\tmemory: ~%.1f MB\n", 9.0 * elements / 1024 / 1024);
     assert(elements <= (num_classes * num_classes));
-    float guess_pop_per = (float) N / (15 - bits / 5) / num_classes;
+    float guess_pop_per = (float) N / (14 - bits / 9) / num_classes;
     printf("\tpopulation per residual ~%.0f\n", guess_pop_per);
+
+
+    /**
+     * Large values reduce Hash size BUT increase number of iterations over X
+     * Aim for a 0.5-2 values of y per pass?
+     */
+    float X_per = sqrt(N / 3.0);
+    size_t num_passes = 1 + 2 * guess_pop_per / X_per;
+    printf("\tX_per ~%.0f -> %lu passes\n", X_per, num_passes);
 
     uint64_t population = 0;
     uint64_t enumerated = 0;
 
     const uint64_t CPU_SPLIT = 128;
     // Outer loop to parallel without contention on Set
-    #pragma omp parallel for schedule(dynamic, 1)
+    //#pragma omp parallel for schedule(dynamic, 1)
     for (size_t v = 0; v < CPU_SPLIT; v++) {
         // Allocated once here to avoid lots of memory allocation.
         Set found;
-        found.reserve(guess_pop_per / 0.3);
+        //found.reserve(guess_pop_per / 0.3);
 
-        //vector<uint64_t> backing_vector;
-        //backing_vector.reserve(guess_pop_per / 0.3);
+        uint64_t iter_cpu = 0;
+        auto start_cpu = std::chrono::steady_clock::now();
 
         for (size_t m = v; m < num_classes; m += CPU_SPLIT) {
             found.clear();
 
-            //Set found(std::less<uint64_t>(), std::move(backing_vector));
-
             auto [f_class, e_class] = expand_class(
-                N, num_classes, m,
+                N, num_classes, m, num_passes,
                 found,
                 classes[m]);
 
-            #pragma omp critical
+            //#pragma omp critical
             {
                 population += f_class;
                 enumerated += e_class;
-
-                // For Maps
-                //population += found.size();
-
-                // For vector
-                //std::sort(found.begin(), found.end());
-                //population +=  std::unique(found.begin(), found.end()) - found.begin();
-
-                // For priority queue
-                // uint64_t last = 0;
-                // while(!found.empty()) {
-                //     population += found.top() != last;
-                //     last = found.top();
-                //     found.pop();
-                // }
+                iter_cpu += e_class;
             }
         }
+
+        if (v <= 10) {
+            auto end = std::chrono::steady_clock::now();
+            double elapsed = std::chrono::duration<double>(end-start_cpu).count();
+            printf("\t\t%2lu, iters: %-12lu  iter/s: %.2f million\n",
+                v, iter_cpu, iter_cpu / 1e6 / elapsed);
+        }
+
     }
 
     // 0 doesn't count for this sequence.
