78a672c40bfe3da6cd4b1204d1d87a53c1dce789
Seth Troisi <sethtroisi@google.com>
2022-05-11 02:37:54 -0700

    error_search v1

---
+++ error_search/README.md
@@ -0,0 +1,30 @@
+# Searching for errors in OEIS
+
+See
+
+* https://math.stackexchange.com/questions/2193384/i-think-i-found-an-error-in-a-oeis-sequence-what-is-the-proper-site-to-post-it
+* http://oeis.org/wiki/Things_to_do_on_the_OEIS#Errors_to_correct
+* https://davidbieber.com/snippets/2020-06-28-oeis-download/
+
+Inspect
+
+* https://oeis.org/A064699 -> 4847 seems wrong
+* https://oeis.org/A078860 -> 169 seems wrong
+* https://oeis.org/A126788 -> 51 seems wrong
+* https://oeis.org/A138000 -> 1797439359 seems wrong
+* https://oeis.org/A145985 -> 753 seem wrong
+* https://oeis.org/A177378 -> 67108861 seem wrong
+* https://oeis.org/A195258 -> 7173 seem wrong (and probably related sequence https://oeis.org/A187825)
+* https://oeis.org/A212605 -> 351439 seem wrong
+* https://oeis.org/A228851 -> 1037917517 seem wrong (and probably related sequences)
+* https://oeis.org/A248526 -> 53687090 seem wrong
+* https://oeis.org/A279192 -> 692 seem wrong
+
+TODO
+
+* https://oeis.org/A237579 -> 2564940997072 should be 25649409970727
+* https://oeis.org/A257110 - Prime sequence contained a composite (48315633)
+* https://oeis.org/A327914 -> 901 should be 907
+
+Fixed / Found
+
---
+++ error_search/quick.py
@@ -0,0 +1,91 @@
+"""
+Search for sequences with all prime/composite EXCEPT for a single term (in the middle)
+"""
+import pprint
+import gmpy2
+
+
+IGNORE = {
+    # First pass with all but one index are prime
+    'A029911', 'A029913', 'A035209', 'A039786', 'A043298', 'A046414',
+    'A047811', 'A054799', 'A054964', 'A055114', 'A056955', 'A059814',
+    'A064699', 'A068871', 'A070159', 'A073319', 'A074647', 'A076033',
+    'A078447', 'A078860', 'A082595', 'A082700', 'A082843', 'A082869',
+    'A089472', 'A095841', 'A099194', 'A106317', 'A107290', 'A109853',
+    'A110074', 'A113910', 'A116974', 'A121837', 'A123976', 'A124112',
+    'A125635', 'A126788', 'A133323', 'A137189', 'A138000', 'A141250',
+    'A141414', 'A143386', 'A144671', 'A145985', 'A160227', 'A167236',
+    'A167806', 'A175189', 'A175524', 'A177378', 'A179538', 'A181659',
+    'A185720', 'A186884', 'A191611', 'A192141', 'A192229', 'A195258',
+    'A200325', 'A203304', 'A208494', 'A209930', 'A212605', 'A212912',
+    'A214032', 'A214788', 'A214791', 'A214792', 'A214794', 'A216059',
+    'A216538', 'A216827', 'A217789', 'A226977', 'A227627', 'A227757',
+    'A228811', 'A228851', 'A236213', 'A237579', 'A239058', 'A239278',
+    'A240102', 'A240103', 'A240915', 'A240960', 'A248526', 'A250291',
+    'A250292', 'A254713', 'A255420', 'A256170', 'A257110', 'A257771',
+    'A259255', 'A260408', 'A260940', 'A262086', 'A263874', 'A263925',
+    'A265653', 'A269983', 'A270449', 'A270834', 'A272069', 'A272649',
+    'A273064', 'A273377', 'A273460', 'A275274', 'A276435', 'A276495',
+    'A279192', 'A280945', 'A292510', 'A293712', 'A294717', 'A295425',
+    'A303604', 'A306661', 'A307512', 'A320641', 'A321866', 'A322394',
+    'A324256', 'A326715', 'A327914', 'A327915', 'A330225', 'A331948',
+    'A334814', 'A335883', 'A335976', 'A336144', 'A340418', 'A342566',
+    'A343829', 'A348883', 'A350402', 'A351398'
+}
+
+
+def test(seq, terms):
+    # TODO might be nice to add cache infront of terms to handle repeat terms
+    is_prime = list(map(gmpy2.is_prime, terms[1:]))
+
+    not_prime = [t for t, is_p in zip(terms[1:], is_prime) if not is_p]
+    is_mostly_prime = len(terms) > 10 and is_prime.count(False) == 1 and (set(not_prime) - {-1, 0, 1, 4})
+
+    not_composite = [t for t, is_p in zip(terms[1:], is_prime) if is_p]
+    is_mostly_composite = len(terms) > 10 and is_prime.count(True) == 1 and (set(not_composite) - {-2, 2, 3, 5})
+
+    for cond, wrong in [
+        (is_mostly_prime, not_prime),
+        (is_mostly_composite, not_composite)
+    ]:
+        if cond:
+            url = f"https://oeis.org/{seq}"
+            if False:
+                print(url)
+            else:
+                print(f"Inspect {seq}: https://oeis.org/{seq}")
+                print("\t:", terms)
+                print("\t:", is_prime)
+                print("\t:", wrong)
+                print("*", url, "->", ', '.join(map(str, wrong)), "seem wrong")
+                print()
+            return True
+
+    return False
+
+
+
+def parse():
+    temp = []
+
+    # See https://davidbieber.com/snippets/2020-06-28-oeis-download/
+    with open("data") as f:
+        for line in f.readlines():
+            if line.startswith("#"):
+                continue
+            line = line.strip("\n, ").split(",")
+            seq, *terms = line
+            seq = seq.strip()
+            if seq in IGNORE:
+                continue
+
+            terms = list(map(int, terms))
+            assert terms, (seq, line)
+            if test(seq, terms):
+                temp.append(seq)
+
+    pprint.pprint(temp, indent=4)
+
+
+if __name__ == "__main__":
+    parse()
