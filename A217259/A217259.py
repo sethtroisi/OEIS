250c63e69f25cb631d93c381efe9394e23ee1c34
Seth Troisi <sethtroisi@google.com>
2022-10-13 18:06:42 -0700

    A217259: Improved python code

--- A217259/A217259.py
+++ A217259/A217259.py
@@ -1,11 +1,6 @@
 import array
-import itertools
 import math
 import time
-from multiprocessing import Pool
-
-from sympy.ntheory.primetest import isprime
-from sympy.ntheory import divisor_sigma
 
 def SegmentedSieveOfSigma(start, N):
     """Calculate sigma[i] for i in range(start, start+n)."""
@@ -61,45 +56,24 @@ def print_match(mid):
         print(f"{found:<8} {mid:<13,}\t{elapsed:.1f} elapsed {rate:.1f}M/s")
         next_time += 5
 
-    if mid in (435, 8576, 8826):
-        return
-
-    assert isprime(mid - 1), mid
-    assert isprime(mid + 1), mid
 
 def iterate(START, STOP, SEGMENT):
-    # sigma "-2, -1"
-    if START <=  1:
-        last_sigmas = [-100, -100]
-    elif START == 1:
-        last_sigmas = [-100, divisor_sigma(1)]
-    else:
-        last_sigmas = [divisor_sigma(START-2), divisor_sigma(START-1)]
+    for start in range(START, STOP, SEGMENT):
+        sigmas = SegmentedSieveOfSigma(start, SEGMENT+2)
+        for i in range(0, SEGMENT):
 
-    for start in itertools.count(START, SEGMENT):
-        sigmas = SegmentedSieveOfSigma(start, SEGMENT)
+            if sigmas[i+2] - sigmas[i] == 2:
+                print_match(start + i + 1)
 
-        if start > STOP:
-            break
+                # Weird non prime terms
+                mid = i + 1
+                if mid in (435, 8576, 8826):
+                    continue
 
-        if sigmas[0] - last_sigmas[0] == 2:
-            print_match(start - 1)
-
-        if sigmas[1] - last_sigmas[1] == 2:
-            print_match(start)
-
-        for i in range(1, SEGMENT-1):
-            if sigmas[i+1] - sigmas[i-1] == 2:
-                print_match(start + i)
-
-        last_sigmas = list(sigmas[-2:])
-        del sigmas
+                assert sigmas[i+2] == (start+i+3), (mid+1, sigmas[i+2])
+                assert sigmas[i] == (start+i+1),   (mid-1, sigmas[i])
 
 START = 0
 SEGMENT = 2 ** 17
-STOP = 10 ** 15
+STOP = 10 ** 9
 iterate(START, STOP, SEGMENT)
-
-# N < 10^8 | 5.3 seconds w/ SEGMENT = 2^16 | 440315 99999588
-# N < 10^9 | 48  seconds w/ SEGMENT = 2^16 | 3424509 999999192
-# N < 10^9 | 56  seconds w/ SEGMENT = 2^20 | 3424509 999999192
