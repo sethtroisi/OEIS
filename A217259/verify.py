547d6c44e2dcd7722c0fad953c2a4607bdb3c769
Seth Troisi <sethtroisi@google.com>
2022-10-16 00:14:13 -0700

    A217259: Added python verify script

--- A217259/verify.py
+++ A217259/verify.py
@@ -1,15 +1,31 @@
 import sys
 import primesieve
 import gmpy2
+import sympy
 
 assert len(sys.argv) == 2, "Must pass STOP"
 
-STOP = int(sys.argv[1])
-print("Running up to", STOP)
+STOP = int(sys.argv[1]) - 1
+MAX_DIST = 12
 
+print("Running up to", STOP, "checking up to", MAX_DIST-1, "away")
+print()
+
+
+print("Checking near STOP")
+sigmas = {a: sympy.ntheory.factor_.divisor_sigma(a) for a in range(STOP-2, STOP+MAX_DIST+1)}
+for a in range(min(sigmas), STOP+1):
+    sigma_a = sigmas[a]
+    for d in range(2, MAX_DIST):
+        if (a + d) in sigmas and (sigma_a + d == sigmas[a + d]):
+            print(f"Match near STOP, sigma({a}) = sigma({a + d}) = {sigma_a}")
+print("Finished STOP checks")
+print()
+
+# Verify my understanding of primesieve endpoint inclusion
 assert 11 in primesieve.primes(11)
 
-counts = [0 for i in range(12)]
+counts = [0 for i in range(MAX_DIST+1)]
 for p in primesieve.primes(STOP):
     counts[0] += 1
     for d in (2,4,6,8,10):
