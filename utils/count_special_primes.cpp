fdb12cddc7796278a687c349c4c841bbd30682de
Seth Troisi <sethtroisi@google.com>
2026-03-14 03:59:31 -0700

    count_population_quadratic_form: Added parallelism
    * Working on vectorized __get_special_prime_counts
    * Parellized count_in_ex by initial prime

--- utils/count_special_primes.cpp
+++ utils/count_special_primes.cpp
@@ -32,8 +32,8 @@ using std::vector;
  */
 
 
-vector<pair<uint64_t, pair<uint64_t, uint64_t>>>
-__get_special_prime_counts(
+vector<uint64_t>
+__get_special_prime_counts_vectorized(
         const uint64_t n, const uint32_t r,
         uint32_t start_prime,
         std::function< uint64_t(uint64_t)> init_count_a,
@@ -44,16 +44,22 @@ __get_special_prime_counts(
     // for i = 1, 2, ..., n/r, n/(r-1), n/(r-2), ... n/3 n/2 n/1
 
     // NOTE: Can technically drop "v" from vector (requires compute v in inner loop)
-    vector<pair<uint64_t, pair<uint64_t, uint64_t>>> counts_backing;
+    const auto length = ((n/r)-1) + r;
+    vector<uint64_t> counts_backing_v;
+    vector<uint64_t> counts_backing_a;
+    vector<uint64_t> counts_backing_b;
     {
-        size_t size = r + n/r - 1;
-        counts_backing.reserve(size);
+        counts_backing_v.reserve(length);
+        counts_backing_a.reserve(length);
+        counts_backing_b.reserve(length);
         // 1, 2, ... n / r - 1
         for(uint32_t v = 1; v < (n / r); v++) {
             uint64_t c_a = init_count_a(v);
             uint64_t c_b = init_count_b(v);
             assert(c_a + c_b <= v);
-            counts_backing.push_back({v, {c_a, c_b}});
+            counts_backing_v.push_back(v);
+            counts_backing_a.push_back(c_a);
+            counts_backing_b.push_back(c_b);
         }
 
         // n/r, n/(r-1), n/(r-2), ... n/3 n/2 n/1
@@ -62,12 +68,12 @@ __get_special_prime_counts(
             uint64_t c_a = init_count_a(v);
             uint64_t c_b = init_count_b(v);
             assert(c_a + c_b <= v);
-            counts_backing.push_back({v, {c_a, c_b}});
+            counts_backing_v.push_back(v);
+            counts_backing_a.push_back(c_a);
+            counts_backing_b.push_back(c_b);
         }
     }
-
-    const auto length = ((n/r)-1) + r;
-    assert(counts_backing.size() == length);
+    assert(counts_backing_v.size() == length);
 
     // Do calculation | 98% of the work is here
     {
@@ -78,95 +84,54 @@ __get_special_prime_counts(
         for (; prime <= r; prime = it.next_prime()) {
             uint64_t p2 = prime * prime;
 
-            const auto& [v, c__] = counts_backing[prime-2];
-            assert(v == prime-1);
-            const auto [c_a, c_b] = c__;
+            const auto outer_v = counts_backing_v[prime-2];
+            auto c_a = counts_backing_a[prime-2];
+            auto c_b = counts_backing_b[prime-2];
+            assert(outer_v == prime-1);
 
             // Index of last term < p2
             uint64_t stop_i = ((p2-1) < r) ? (p2-2) : (length - ((n-1) / p2 + 1));
-            assert(counts_backing[stop_i].first < p2);
-            assert(counts_backing[stop_i+1].first >= p2);
+            assert(counts_backing_v[stop_i] < p2);
+            assert(counts_backing_v[stop_i+1] >= p2);
 
             /**
              * Updating counts of v in [p^2, n] referencing v/p
              * if p^3 > N, then n/p isn't updated during the loop so safe to do at the same time.
              */
             if (is_group_a(prime)) {
-                for (size_t i = counts_backing.size() - 1; i > stop_i; i--) {
-                    auto& [v, u] = counts_backing[i];
+                for (size_t i = counts_backing_v.size() - 1; i > stop_i; i--) {
+                    const auto v = counts_backing_v[i];
                     assert(v >= p2);
 
-                    // Only need v for this t
                     uint64_t t = v / prime;
-                    // t = (n / i) / prime
                     size_t index = (t < r) ? (t-1) : (length - (n / t));
-                    assert(counts_backing[index].first == t);
+                    assert(counts_backing_v[index] == t);
 
-                    const auto temp = counts_backing[index].second;
-                    u.first  -= temp.first  - c_a;
-                    u.second -= temp.second - c_b;
+                    counts_backing_a[i] -= counts_backing_a[index] - c_a;
+                    counts_backing_b[i] -= counts_backing_b[index] - c_b;
                 }
             } else {
-                for (size_t i = counts_backing.size() - 1; i > stop_i; i--) {
-                    auto& [v, u] = counts_backing[i];
+                for (size_t i = counts_backing_v.size() - 1; i > stop_i; i--) {
+                    const auto v = counts_backing_v[i];
                     assert(v >= p2);
 
                     uint64_t t = v / prime;
                     size_t index = (t < r) ? (t-1) : (length - (n / t));
-                    assert(counts_backing[index].first == t);
+                    assert(counts_backing_v[index] == t);
 
-                    const auto temp = counts_backing[index].second;
-                    u.first  -= temp.second  - c_b;
-                    u.second -= temp.first   - c_a;
+                    counts_backing_a[i] -= counts_backing_b[index] - c_b;
+                    counts_backing_b[i] -= counts_backing_a[index] - c_a;
                 }
             }
+
+            // Write results back to counts_backing_{a,b}
+            counts_backing_a[prime-2] = c_a;
+            counts_backing_b[prime-2] = c_b;
         }
     }
-    return counts_backing;
+
+    return counts_backing_b;
 }
-
-
-// /**
-//  * Get number of primes <= i for important values of i.
-//  * Assumes primes are in two congruence classes.
-//  *
-//  * See older code in A000047/A000205 for concrete examples
-//  * (number of primes % 8 in (5,7)) <= i for important values of i
-//  *
-//  * Adapted from Lucy_Hedgehog's post in Problem 10
-//  * https://projecteuler.net/thread=10;page=5#111677
-//  * https://math.stackexchange.com/a/2283829/87805
-//  */
-// Map<uint64_t, uint64_t>
-// get_special_prime_counts(
-//         uint64_t n, uint32_t r,
-//         uint32_t start_prime,
-//         std::function< uint64_t(uint64_t)> init_count_a,
-//         std::function< uint64_t(uint64_t)> init_count_b,
-//         std::function< bool(uint64_t)> is_group_a
-// ) {
-//     auto start = std::chrono::high_resolution_clock::now();
-//
-//     auto counts_backing = __get_special_prime_counts(
-//         n, r, start_prime,
-//         init_count_a, init_count_b, is_group_a);
-//
-//     // Grab result in format we want
-//     Map<uint64_t, uint64_t> count_primes;
-//     count_primes.reserve(counts_backing.size() / 0.7);
-//     for (auto& [i, backing] : counts_backing) {
-//         //fprintf(stderr, "\t%lu\t%lu  %lu\n", i, backing.first, backing.second);
-//         count_primes[i] = backing.second;
-//     }
-//
-//     {
-//         auto end = std::chrono::high_resolution_clock::now();
-//         double elapsed = std::chrono::duration<double>(end - start).count();
-//         fprintf(stderr, "\tcount_special_primes(%lu) = %lu  (%.2f)\n",
-//                 n, count_primes.at(n), elapsed);
-//     }
-//     return count_primes;
-// }
 
 
 /**
@@ -185,24 +150,10 @@ get_special_prime_counts_vector(
 ) {
     auto start = std::chrono::high_resolution_clock::now();
 
-    auto counts_backing = __get_special_prime_counts(
+    auto count_primes = __get_special_prime_counts_vectorized(
         n, r, start_prime,
         init_count_a, init_count_b, is_group_a);
 
-    assert(counts_backing[0].first == 1);      // First value should be 1
-    assert(counts_backing.back().first == n);  // Last value should be n
-
-    /**
-     * Reduces 3x uint64 to 1x uint64
-     * BUT temporarily increases memory pressure
-     *
-     * Possibly I can read/erase from back, then reserve
-     */
-    vector<uint64_t> count_primes;
-    count_primes.resize(counts_backing.size());
-    for (size_t i = 0; i < counts_backing.size(); i++) {
-        count_primes[i] = counts_backing[i].second.second;
-    }
     // counts should be increasing
     assert(is_sorted(count_primes.begin(), count_primes.end()));
 
@@ -215,6 +166,25 @@ get_special_prime_counts_vector(
     return count_primes;
 }
 
+uint64_t isqrt(uint64_t x) {
+    /* Power of 4 greater or equal to x */
+    uint64_t q = 1;
+    while (q <= x)
+        q <<= 2;
+
+    /* Quadaratic residue */
+    uint64_t r = 0;
+    while (q > 1) {
+        q >>= 2;
+        int64_t t = x - r - q;
+        r >>= 1;
+        if (t >= 0) {
+            x = t;
+            r += q;
+        }
+    }
+    return r;
+}
 
 /**
  * Count population of 2^n of quadratic form generated by [2][p][q]^2
@@ -231,18 +201,18 @@ uint64_t count_population_quadratic_form(
         std::function< uint64_t(uint64_t)> init_count_b,
         std::function< bool(uint64_t)> is_group_a
 ) {
+    assert (bits <= 62); // overflows probably exist
+                         //
     uint64_t n = 1ul << bits;
-    uint64_t r = sqrt(n) + 1;
-    while (r*r > n) {
-        r--;
-    }
+    uint64_t r = isqrt(n);
+    uint64_t fourth_root = isqrt(r);
     assert(r*r <= n);
     assert((r+1) * (r+1) > n);
     assert(r < std::numeric_limits<uint32_t>::max());
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    // 40-60% of time is building special prime counts.
+    // 50-65% of time is building special prime counts.
     const auto count_special_primes = get_special_prime_counts_vector(
         n, r,
         start_prime,
@@ -294,6 +264,7 @@ uint64_t count_population_quadratic_form(
             return n;
         }
 
+
         uint64_t count = n;
 
         // Handle p <= n < p^2
@@ -330,7 +301,7 @@ uint64_t count_population_quadratic_form(
     };
 
     std::function<uint64_t(uint64_t, uint32_t, uint8_t)> count_in_ex;
-    count_in_ex = [&special_primes, &count_in_ex, &count_in_ex_large]
+    count_in_ex = [&special_primes, &count_in_ex, &count_in_ex_large, fourth_root]
                       (uint64_t n, uint32_t pi, uint8_t root) {
         if (n < special_primes[pi])
             return n;
@@ -341,12 +312,7 @@ uint64_t count_population_quadratic_form(
           // Handle p where p^4 <= n
           for (; pi < special_primes.size(); pi++) {
               uint64_t p = special_primes[pi];
-              uint64_t p2 = p * p;
-              // Do I need to worry about overflow (on first index) for recursive calls?
-              // Could also trigger if overflow happens
-
-              //  if p^4 > n, break. this loop is for p^4 <= n
-              if (p2 > n / p2)
+              if (p > fourth_root)
                   break;
 
               // This loop has been optimized see A000047.py, for clearer code
@@ -429,9 +395,29 @@ uint64_t count_population_quadratic_form(
         return count;
     };
 
-    /* FIXME: This could be parallelized with a parallel pi start
-     * and restricting pi during recursion*/
-    uint64_t count = count_in_ex(n, 0, 100);
+    // in parallel handle all small primes
+    uint64_t count = 0;
+    if (1) {
+        // Would be nice to break this into two sections with larger chunk in the later section.
+        #pragma omp parallel for schedule(dynamic, 1) reduction(+:count)
+        for (uint64_t pi = 0; pi < special_primes.size() - 2; pi++) {
+            uint64_t p = special_primes[pi];
+            assert(p <= r);
+            uint64_t pp = p;
+            uint64_t n_pp = n / pp;
+            for (; n_pp ; ) {
+                count -= count_in_ex(n_pp, pi+1, 100);
+                n_pp /= p;
+                count += count_in_ex(n_pp, pi+1, 100);
+                n_pp /= p;
+            }
+        }
+        uint64_t pi = special_primes.size() - 2;
+        assert(special_primes[pi] > r); // special primes always has 2 > r at the end
+        count += count_in_ex_large(n, pi);
+    } else {
+        count = count_in_ex(n, 0, 100);
+    }
 
     {
         auto end = std::chrono::high_resolution_clock::now();
