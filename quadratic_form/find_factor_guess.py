57f007b9a89431777034669fff3493980507082f
Seth Troisi <sethtroisi@google.com>
2026-03-17 12:24:08 -0700

    more quadratic_form exploration

--- quadratic_form/find_factor_guess.py
+++ quadratic_form/find_factor_guess.py
@@ -1,4 +1,5 @@
 """Find number of a quadratic form, Factor them, guess at prime rules"""
+import primesieve
 import sympy
 
 def brute(a, b, limit=2**20):
@@ -11,13 +12,60 @@ def brute(a, b, limit=2**20):
     return sorted(nums)
 
 
+def build(primes_p, primes_q, limit=10000):
+    """
+    Build numbers of the form [p][q]^2
+    """
+    def expand(primes):
+        t = []
+        def recursive(n, i):
+            if i > limit:
+                return
+            t.append(n)
+            for pi in range(i, len(primes)):
+                p = primes[pi]
+                tn = n * p
+                if tn > limit:
+                    break
+                recursive(tn, pi)
+
+        recursive(1, 0)
+        return sorted(t)
+
+    def merge(A, B):
+        m = []
+        for a in A:
+            for b in B:
+                n = a * b
+                if n > limit:
+                    break
+                m.append(n)
+        m.sort()
+        return m
+
+    partial_p = expand(primes_p)
+    partial_q = expand([p*p for p in primes_q])
+    print("\tprimes_p:", primes_p[:10], "->", partial_p[:10])
+    print("\tprimes_q:", primes_q[:10], "->", partial_q[:10])
+    found = merge(partial_p, partial_q)
+    print("\tfound:", len(found), found[:20])
+    found2 = merge(found, [2 ** i for i in range(30)])
+    print("\tfound2:", len(found2), found2[:20])
+    return found, found2
+
+
 def factor(nums):
     # Primes found to even and odd powers
     primes_even = set()
     primes_odd = set()
     for n in nums:
+        if n <= 2: continue
+
         f = sympy.factorint(n)
         for p, e in f.items():
+            # p is a weird prime
+            if p == 2: continue
+
             if e % 2 == 0:
                 primes_even.add(p)
             else:
@@ -26,12 +74,13 @@ def factor(nums):
     both = primes_even & primes_odd
     xor = primes_even ^ primes_odd
 
+    print("Nums:\t", sorted(nums)[:25])
     print("Even:\t", sorted(primes_even)[:20])
     print("Odd:\t", sorted(primes_odd)[:20])
     print("&:\t", sorted(both)[:20])
     print("^:\t", sorted(xor)[:20])
 
-    for mod in [6, 8]:
+    for mod in [4, 6, 8]:
         print(f"  Even % {mod}:\t", sorted(set(p % mod for p in primes_even)))
         print(f"  Odd % {mod}:\t", sorted(set(p % mod for p in primes_odd)))
         print("  &:\t\t", sorted(set(p % mod for p in both))[:20])
@@ -42,20 +91,23 @@ def run(a, b):
     nums = brute(a, b)
     factor(nums)
 
+special_forms = {
+    "A000047": (1, -2), # Known to have special form %8 = {1,7} | {3,5}
+    "A000050": (1, 1), # Known to have special form %4 = 1 | 3
+    "A000067": (1, 2), # Known to have special form %8 = {1,3} | {5,7}
+    "A000205": (1, 3), # Known to have special form %6 = {1,3} | {2,5}
+}
+
 forms = {
-    "A000018": (1, 16),
+    "A000018": (1, 16),  # x^2 + 16 y^2
     "A000021": (1, 12),
-    "A000024": (1, 10),  # x^2 + 10 y^2
-    "A000047": (1, -2),  # x^2 - 2 y^2
-    "A000049": (3, 4),   # 3 x^2 + 4 y^2
-    "A000050": (1, 1),
-    "A000067": (1, 2),
+    "A000024": (1, 10),
+    "A000049": (3, 4),
     "A000072": (1, 4),
     #"A000074": (1, 4),   # Odds only
     "A000075": (2, 3),
     "A000076": (4, 4, 5), # 4 x^2 + 4 x y + 5 y^2
     "A000077": (1, 6),
-    "A000205": (1, 3),
     "A000286": (2, 5),
     "A054150": (1, 5),
     "A054151": (1, 7),
@@ -88,11 +140,40 @@ forms = {
     "A054191": (8, 9),
     "A054193": (9, 9),
     "A054194": (9, 10),
-
 }
 
-for seq, form in forms.items():
-    print(seq, form)
-    if len(form) == 2:
-        run(*form)
-    print("\n")
+if 1:
+    test = special_forms # | forms
+    for seq, form in special_forms.items():
+        print(seq, form)
+        if len(form) == 2:
+            run(*form)
+        print("\n")
+        break
+
+
+if 0:
+    for e in range(2, 10):
+        L = 2 ** e
+        primes = primesieve.primes(L)
+
+        # A000047
+        #p_a = [p for p in primes if p % 8 in (1, 7)]
+        #p_b = [p for p in primes if p % 8 in (3, 5)]
+
+        # A000050
+        #p_a = [p for p in primes if p % 4 == 1]
+        #p_b = [p for p in primes if p % 4 == 3]
+
+        # A000067
+        #p_a = [p for p in primes if p % 8 in (1, 3)]
+        #p_b = [p for p in primes if p % 8 in (5, 7)]
+
+        # A000205 result is found1 not found2
+        #p_a = [p for p in primes if p % 6 in (1, 3)]
+        #p_b = [p for p in primes if p % 6 in (2, 5)]
+
+
+        f1, f2 = build(p_a, p_b, limit=L)
+        print(e, len(f1), len(f2))
+
